810a6ef322d5f43855c8dee4860dbabeddfa8c61
Sandy Carter <bwrsandman@gmail.com>
2020-02-05 21:17:08 +0100

    factor out mesh creation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <cstdlib>
 #include <SDL.h>
 #include <glad/glad.h>
-#include <memory>
 #include <string_view>
 
+#include "IndexedMesh.h"
 #include "Renderer.h"
 
 //just copy a glsl file in here with the vertex shader
@@ -26,60 +26,16 @@ constexpr std::string_view fragmentShaderSource = "#version 460 core\n"
                                             "    color = vec4(textureCoordinate.x, textureCoordinate.y, 0, 1);\n"
                                             "}\n";
 
-
-
 int main() {
     int screenWidth = 800;
     int screenHeight = 600;
-
-    float quad_vertices[] = {
-            0.f, 0.f, // 0 top left
-            1.0, 0.f, // 1 top right
-            1.f, 1.f, // 2 bottom right
-            0.f, 1.f, // 3 bottom left
-    };
-    static const uint16_t quad_indices[6] = {0, 1, 2, 2, 3, 0}; //drawing the quad
 
     auto renderer = Renderer::create("Calibration", 800, 600);
     if (!renderer) {
       return EXIT_FAILURE;
     }
 
-    uint32_t quadVertexBuffer;
-    glCreateBuffers(1, &quadVertexBuffer); //create buffer pointer on gpu
-    uint32_t indexBuffer;
-    glCreateBuffers(1, &indexBuffer);
-    //you can also create them at once if you have a bufferarray, then put 2
-
-	glObjectLabel(GL_BUFFER, quadVertexBuffer, -1, "fullscreen quad vertexbuffer"); //to be able to read it in renderdoc/errors
-	glObjectLabel(GL_BUFFER, indexBuffer, -1, "fullscreen quad indexbuffer"); //to be able to read it in renderdoc/errors
-
-
-    uint32_t vao;
-    glCreateVertexArrays(1, &vao);
-	glObjectLabel(GL_VERTEX_ARRAY, vao, -1, "fullscreen quad vertex array object"); //to be able to read it in renderdoc/errors
-
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVertexBuffer); //binds buffers to the slot in the vao, and this makes no sense but is needed somehow
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-	//tell renderdoc (and the gpu) you use float2 for the vertices at position 0 in the shader
-	glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, nullptr); //define array dimensionality and also say you step per two floats
-	glEnableVertexAttribArray(0);
-
-    glNamedBufferData(quadVertexBuffer, sizeof(quad_vertices), nullptr, GL_STATIC_DRAW); // malloc without data, reserve space on gpu
-    glNamedBufferData(indexBuffer, sizeof(quad_indices), nullptr, GL_STATIC_DRAW);
-
-    {
-        void* mappedMemory = glMapNamedBuffer(quadVertexBuffer, GL_WRITE_ONLY);
-        memcpy(mappedMemory, quad_vertices, sizeof(quad_vertices));
-        glUnmapNamedBuffer(quadVertexBuffer);
-    }
-    {
-        void *mappedMemory = glMapNamedBuffer(indexBuffer, GL_WRITE_ONLY);
-        memcpy(mappedMemory, quad_indices, sizeof(quad_indices));
-        glUnmapNamedBuffer(indexBuffer);
-    }
+    auto fullscreenQuad = IndexedMesh::createFullscreenQuad("fullscreen quad");
 
     //pipeline
     uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -167,15 +123,11 @@ int main() {
 
         glUseProgram(fullScreenBlit);
 
-        glBindVertexArray(vao);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, nullptr); //tell it you want to draw 2 triangles (2 vertices)
+        // tell it you want to draw 2 triangles (2 vertices)
+        fullscreenQuad->draw();
 
         renderer->swapBuffers();
     }
     glDeleteProgram(fullScreenBlit);
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &indexBuffer);
-    glDeleteBuffers(1, &quadVertexBuffer);
     return EXIT_SUCCESS;
 }
-
